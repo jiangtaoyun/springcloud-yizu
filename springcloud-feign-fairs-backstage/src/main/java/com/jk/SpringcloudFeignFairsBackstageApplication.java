ec43df9b8e8360a4a21a1b78cc09890d360cf313
gtt19960308 <980234538@qq.com>
2019-07-14 18:26:59 +0800

    gttonce

---
+++ springcloud-feign-fairs-backstage/src/main/java/com/jk/SpringcloudFeignFairsBackstageApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudFeignFairsBackstageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudFeignFairsBackstageApplication.class, args);
+    }
+
+}
